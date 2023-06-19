5f1ae893fff31e177cc999655f38c1fd2a804dd8
MiguelGarcia23 <alejandro23vzla@gmail.com>
2023-06-19 15:57:08 -0400

    Creando la página de Hablemos

--- front-end/src/pages/ContactPage.jsx
+++ front-end/src/pages/ContactPage.jsx
@@ -1,22 +1,27 @@
 /* Importando componentes propios */
 import NavbarMobile from '../components/navbar/NavbarMobile';
 import NavbarDesktop from '../components/navbar/NavbarDesktop';
-
-/* Importando estilos del Contact */
-import '../css/Contact.css';
+import FormContact from '../components/partials/FormContact';
+import Footer from '../components/partials/Footer';
 
 
 const ContactPage = () => {
 
     return (
 
-        <main className='contact-page'>
+        <main>
 
             {/* Navbar principal versión Mobile */}
             <NavbarMobile />
 
             {/* Navbar principal versión Desktop */}
             <NavbarDesktop />
+
+            {/* Sección de Contacto */}
+            <FormContact />
+
+            {/* Footer */}
+            <Footer />
 
         </main>
 
